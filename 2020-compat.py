c477ba2ec3c03b327d90e7c51a491618e07a0921
Sebastião Miranda <sebastiao.miranda@outlook.com>
2019-12-10 17:45:12 +0000

    Create 2020-compat.py

---
+++ 2020-compat.py
@@ -0,0 +1,7 @@
+# WIP - compatibility script to prepare owlas for 2020
+# NOTE - should be a single file so that it doesn't screw up the printer thingy we did to read code on paper.
+# TODO - @tomf, please add dojo/promise code ASAP.
+
+int main() {
+  // WIP
+}
